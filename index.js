e0118d1800e95bf30e1e4758e1ef07de5903384e
Youssef Chakir <69270797+Rango-bom@users.noreply.github.com>
2024-05-20 13:38:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+let movieNameRef = document.getElementById("search-input");
+let searchButton = document.getElementById("search-button");
+let result = document.getElementById("result");
+//function to fetch data from api
+
+let getMovie = () => {
+
+    let movieName = movieNameRef.value;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=7f9a3b82`;
+
+    //if input field is empty
+
+    if (movieName.length <= 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a movie name </h3>`;
+    }
+
+    //if input isn't empty
+    else {
+        fetch(url).then((resp) => resp.json()).then((data) => {
+
+            //if movie exist in database
+            if (data.Response == "True") {
+                result.innerHTML = `
+
+                    <div class="info">
+                        <img src=${data.Poster} class="poster">
+                        <div>
+                            <h2>${data.Title}</h2>
+                            <div class="rating">
+                                <img src="star-icon.png">
+                                <h4>${data.imdbRating}</h4>
+                            </div>
+                            <div class="details">
+                                <span>${data.Rated}</span>
+                                <span>${data.Year}</span>
+                                <span>${data.Runtime}</span>
+                            </div>
+                            <div class="genre">
+                                <div>${data.Genre.split(",").join("</div><div>")}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <h3>Plot:</h3>
+                    <p>${data.Plot}</p>
+                    <h3>Cast:</h3>
+                    <p>${data.Actors}</p>
+                `;
+            }
+
+            //if movie doesn't exist in database
+            else {
+                result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+            }
+        })
+            //if error occurs
+            .catch(() => {
+                result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+            });
+    }
+};
+
+searchButton.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
